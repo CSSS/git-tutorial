bbc9f715e0cda867e2075a639f89caed31c435b3
nbosco <bnoronha@sfu.ca>
2015-10-25 11:19:31 -0700

    Created collaboration excercises for Linux

---
+++ exercise.py
@@ -0,0 +1,55 @@
+'''
+An exercise to practice collaboration using git and Github.
+Author: Bosco Noronha (nbosco)
+Date: 25th Oct 2015
+'''
+
+
+
+'''
+Purpose of the function:
+Given a array of integers, return the length of the array.
+Eg. Input: [1, 2, 3, 4, 5, 6, 7, 8]
+    Output: 8
+'''
+def amountInArray():
+
+
+
+'''
+Purpose of the function:
+Given a array of integers, return the sum of all the integers.
+Eg. Input: [1, 2, 3, 4, 5, 6, 7, 8]
+    Output:
+'''
+def sumOfArray():
+
+
+'''
+Purpose of the function:
+Given a array of integers, return the array in reverse order.
+Eg. Input: [1, 2, 3, 4, 5, 6, 7, 8]
+    Output:[8, 7, 6, 5, 4, 3, 2, 1]
+'''
+def reverseArray:
+
+
+
+'''
+Purpose of the function:
+Given a array of integers, return the array shuffled.
+Eg. Input: [1, 2, 3, 4, 5, 6, 7, 8]
+    Output:[8, 2, 1, 5, 7, 3, 4, 6]
+'''
+def shuffleArray():
+
+
+
+
+#MAIN
+int array = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print amountInArray()
+print sumOfArray()
+print reverseArray()
+print shuffleArray()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+* A exercise to practice collaboration.
+* Author: Bosco Noronha (nbosco)
+* Date: 25th Oct 2015
+*/
+
+
+
+int main(){
+
+  return 0;
+}
