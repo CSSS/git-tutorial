7d8cb16ef81e13cbe91304dc2d021fe2472dbe25
Bosco Noronha <nbosco182@gmail.com>
2015-10-29 14:02:55 -0700

    Added functions to exercise c++

--- ExerciseVisualStudio/ExerciseVisualStudio/Source.cpp
+++ ExerciseVisualStudio/ExerciseVisualStudio/Source.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+void reverse(int *array) {
+
+}
+
+void sum(int *array) {
+
+}
+
+void total(int *array) {
+
+}
 
 int main() {
+
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 	return 0;
 }
