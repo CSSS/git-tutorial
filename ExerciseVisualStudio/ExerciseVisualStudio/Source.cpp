895889188b0b6184aa9d4131fd7c2d8022d1f338
Bosco Noronha <nbosco182@gmail.com>
2015-11-10 15:52:19 -0800

    Added comments

--- ExerciseVisualStudio/ExerciseVisualStudio/Source.cpp
+++ ExerciseVisualStudio/ExerciseVisualStudio/Source.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
+// Reverse the given array
 void reverse(int *array) {
 
 }
-
+// Find the sum of the elements in the array
 void sum(int *array) {
 
 }
-
+// Find the total number of elements in the array
 void total(int *array) {
+	
+}
+
+// Randomly shuffle the array
+void random(int *array) {
 
 }
 
 int main() {
 
 	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
+	
 	return 0;
 }
